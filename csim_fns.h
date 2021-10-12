53188cc0cc16c22239b124226df07b2b77d77c35
Elizabeth Aufzien <eliauf23@gmail.com>
2021-10-12 14:35:41 -0400

    added block properties

--- csim_fns.h
+++ csim_fns.h
@@ -6,12 +6,15 @@
 #include <stdint.h>
 #include <string.h>
 
-
-
+#define MIN_BLOCK_SIZE 4U
 
 //use unsigned 32 bit integers for any memory addresses/indices
 typedef struct {
-    uint32_t dummyVar;
+    uint32_t dirty; //dirty bit - for write-back cache (tells you that you need to write changes back to memory before evicting block)
+    uint32_t valid; //valid bit
+    uint32_t tag;//tag - tells you the block you're in
+    uint32_t evictionPriority; //higher value indicates that it's not being accessed
+    // for LRU: measure of times set has been accessed, but this block hasn't been
 } Block;
 
 typedef struct {
