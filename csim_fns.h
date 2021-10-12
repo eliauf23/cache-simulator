9c5dcfe8de83478174a32ce18db723b2a37c88b2
Elizabeth Aufzien <eliauf23@gmail.com>
2021-10-12 15:59:03 -0400

    implemented Block, Set & Cache typedefs for MS1, added comments for features we'll likely need for future milestones

--- csim_fns.h
+++ csim_fns.h
@@ -6,7 +6,20 @@
 #include <stdint.h>
 #include <string.h>
 
+//define constants:
+
 #define MIN_BLOCK_SIZE 4U
+/* assumptions from assignment description:
+ * this is number of cycles to perform
+ * load/store operation from specified location
+ * to CPU for a single 4 byte quantity
+ */
+#define CACHE_LOAD_CYCLES 1U
+#define CACHE_STORE_CYCLES 1U
+#define MEM_LOAD_CYCLES 100U
+#define MEM_STORE_CYCLES 100U
+#define ADDRESS_LEN 32U
+
 
 //use unsigned 32 bit integers for any memory addresses/indices
 typedef struct {
@@ -18,17 +31,26 @@ typedef struct {
 } Block;
 
 typedef struct {
-        uint32_t dummyVar;
+    uint32_t numBlocks; //number of blocks in this set
+    Block * blocks; //array of blocks
+    uint32_t numUnusedBlocks;
 
 } Set;
 
 typedef struct {
-        uint32_t dummyVar;
-
+    uint32_t numSets; //number of sets in this cache
+    Set * sets; //array of sets
+    uint32_t indexLen; //i.e. num bits in index
+    uint32_t offsetLen; //num bits in offset
+    uint32_t addressLen = ADDRESS_LEN;
+    //not needed in direct mapping, but might need number of blocks per set for associative mapping
 } Cache;
 
 
-//do we want to use c++ so we can define classes?
 
+//prints out results with required formatting
+void displayResults(uint32_t loads, uint32_t stores, uint32_t loadHits, uint32_t loadMisses, uint32_t storeHits, uint32_t storeMisses, uint32_t cycles);
 
+//returns 1 if number is a power of 2, 0 if not a power of 2
+int isPowerOfTwo(uint32_t num);
 #endif
