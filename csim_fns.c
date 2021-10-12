5fb7ef815c0c712f01d7007dded271a76eedf581
Elizabeth Aufzien <eliauf23@gmail.com>
2021-10-12 17:06:26 -0400

    added function implementations for functions declared in header

--- csim_fns.c
+++ csim_fns.c
@@ -3,12 +3,38 @@
 #include <assert.h>
 #include <stdint.h>
 #include <string.h>
-
+#include <limits.h>
 #include "csim_fns.h"
 
 
-#define MIN_BLOCK_SIZE 4U
+int printErrorMsg(char * msg) {
+    fprintf(stderr, "%s\n", msg);
+    return 1; //exit or return?
+}
 
-uint32_t dummyFunction() {
-    return 0;
+void displayResults(uint32_t loads, uint32_t stores, uint32_t loadHits, uint32_t loadMisses, uint32_t storeHits, uint32_t storeMisses, uint32_t cycles) {
+    printf("Total loads: %u\nTotal stores: %u\nLoad hits: %u\nLoad misses: %u\nStore hits: %u\nStore misses: %u\nTotal cycles: %u\n",
+           loads, stores, loadHits, loadMisses, storeHits, storeMisses, cycles);
+}
+
+
+unsigned int isPowerOfTwo(uint32_t num) {
+    return (num && !(num & (num - 1)));
+}
+
+int findAddressInCache(Cache cache, uint32_t address){
+    int something = cache.numSets + address;
+    something = -1;
+    return something;
+}
+
+unsigned int log_base2(unsigned int num) {
+    if (num == 0) return UINT_MAX; //TODO: I think this is preferable to using negatives. Thoughts?
+    if (num == 1) return 0;
+    unsigned int result = 0U;
+    while (num > 1) {
+        num = num >> 1;
+        num++;
+    }
+    return result;
 }
