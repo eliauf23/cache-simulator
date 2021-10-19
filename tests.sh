54c9f023897dbca272bbe3b342af504bf0b3b4da
Elizabeth Aufzien <eliauf23@gmail.com>
2021-10-19 12:34:53 -0400

    added shell script that we can use to test cache configurations

---
+++ tests.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+make clean
+
+make
+
+./csim 256 4 16 write-allocate write-back lru < gcc.trace
+
+
+# echo "enter numSets numBlocks blockSize & filename:\n"
+
+# echo "write-allocate write-through lru \n\n"
+# ./csim $1 $2 $3 write-allocate write-through lru < example_traces/$4 
+
+# echo "write-allocate write-back lru \n\n"
+# ./csim $1 $2 $3 write-allocate write-back lru < example_traces/$4
+
+# echo "no-write-allocate write-through lru \n\n"
+# ./csim $1 $2 $3 no-write-allocate write-through lru < example_traces/$4
+
+# echo "no-write-allocate write-back lru: this configuration should produce an error! \n\n"
+# ./csim $1 $2 $3 no-write-allocate write-through lru < example_traces/$4
