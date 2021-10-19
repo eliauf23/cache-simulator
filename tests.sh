49bd36cee9a631e1a8697e52340e702fb69d0dcd
Elizabeth Aufzien <eliauf23@gmail.com>
2021-10-19 16:14:22 -0400

    added tests for dif cache configs

--- tests.sh
+++ tests.sh
@@ -5,10 +5,12 @@ make clean
 
 make
 
-./csim 256 4 16 write-allocate write-back lru < gcc.trace
+./csim 256 4 16 write-allocate write-back lru < example_traces/write01.trace
 
+#should enter tests for other configurations - direct, N-way assoc, fully assoc
 
 # echo "enter numSets numBlocks blockSize & filename:\n"
+
 
 # echo "write-allocate write-through lru \n\n"
 # ./csim $1 $2 $3 write-allocate write-through lru < example_traces/$4 
