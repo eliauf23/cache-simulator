981ae39547cb2aa54762d2fc75eaf49cf71c633e
Elizabeth Aufzien <eaufzie1@jh.edu>
2021-10-23 13:56:11 -0400

    cleaned up files

--- set.cpp
+++ set.cpp
@@ -1,90 +1,19 @@
 #include "set.h"
 #include "block.h"
 
-
 namespace CacheSimulator
 {
 
-    void Set::addBlock(Block * b)
-    {
-        _blocks.push_back(b);
-         
-    }
-
-
-    void Set::evictLRU(uint32_t evictIdx)
-    {
-        uint32_t lastAccessTime = getBlockAtIndex(evictIdx)->getTime();
-        if (lastAccessTime == 0)
-        {
-            return;
-        }
-
-        for (uint32_t i = 0; i < _numBlocks; i++)
-        {
-            Block *b = getBlockAtIndex(i);
-
-            if (b != nullptr)
-            {
-                if (b->isValid() && b->getTime() <= lastAccessTime)
-                {
-                    b->incrementTime();
-                }
-            }
-        }
-        getBlockAtIndex(evictIdx)->resetTime();
-    }
-
-
-    void Set::evictFIFO(uint32_t evictIdx)
-    {
-        for (uint32_t i = 0; i < _numBlocks; i++)
-        {
-            getBlockAtIndex(i)->incrementTime();
-        }
-        getBlockAtIndex(evictIdx)->resetTime();
-    }
-
-    //generates empty set
-    Set::Set(uint32_t numBlocks) :  _emptyBlocks(numBlocks) ,_numBlocks(numBlocks) {}
-
-    Block* Set::getBlockAtIndex(uint32_t i)
+    Set::Set(uint32_t numBlocks, uint32_t index)
     {
 
-        if (!_blocks.empty() && i < _blocks.size())
-        {
-        
-            return _blocks[i];
-        }
-        else
-        {
-            return nullptr;
-        }
-    }
-
-    bool Set::isEmpty()
-    {
-        return (_numBlocks == 0);
-    }
-
-    uint32_t Set::getNumBlocks() const
-    {
-        return _numBlocks;
-    }
-
-    void Set::setNumBlocks(uint32_t numBlocks)
-    {
         _numBlocks = numBlocks;
-    }
-
-    uint32_t Set::getEmptyBlocks() const
-    {
-        return _emptyBlocks;
-    }
-
-    void Set::setEmptyBlocks(uint32_t emptyBlocks)
-    {
-        _emptyBlocks = emptyBlocks;
+        _emptyBlocks = numBlocks;
+        _setIndex = index;
+        for (uint32_t i = 0; i < numBlocks; i++)
+        {
+            _blocks.push_back(Block());
+        }
     }
 
 }
--- set.h
+++ set.h
@@ -9,33 +9,19 @@
 namespace CacheSimulator
 {
 
-    class Set {
+    class Set
+    {
     public:
-        Set() {};
-        Set(uint32_t numBlocks);
+        Set(uint32_t numBlocks, uint32_t index);
+        // add destructor?
 
-        void addBlock(Block *b);
-
-        void evictLRU(uint32_t i);
-        void evictFIFO(uint32_t i);
-
-        Block* getBlockAtIndex(uint32_t i);
-        bool isEmpty();
-        uint32_t getNumBlocks() const;
-
-        void setNumBlocks(uint32_t numBlocks);
-
-        uint32_t getEmptyBlocks() const;
-
-        void setEmptyBlocks(uint32_t emptyBlocks);
+        std::vector<Block> _blocks;
 
     private:
-        std::vector <Block *> _blocks;
+        uint32_t _setIndex;
         uint32_t _emptyBlocks;
         uint32_t _numBlocks;
-
-
     };
 }
 
-#endif //SET_H
+#endif // SET_H
