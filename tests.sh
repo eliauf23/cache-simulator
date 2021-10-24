7f6b10559b28da70013eb66a607234d8ddf585e6
Elizabeth Aufzien <eaufzie1@jh.edu>
2021-10-23 20:53:53 -0400

    added more tests!

--- tests.sh
+++ tests.sh
@@ -4,12 +4,22 @@ make clean
 
 make
 
-#./csim 256 4 16 write-allocate write-through lru < example_traces/swim.trace
 
-./csim 256 4 16 write-allocate write-back lru < example_traces/read02.trace
-./csim 2 4 16 write-allocate write-back lru < example_traces/read03.trace
-./csim 256 4 16 write-allocate write-through lru < example_traces/write01.trace
-./csim 256 4 16 write-allocate write-back lru < example_traces/write02.trace
+
+
+
+
+./csim 2 2 4 write-allocate write-through lru < example_traces/ex1.trace
+
+
+
+#./csim 256 4 128 write-allocate write-back lru < example_traces/swim.trace
+#./csim 256 16 64 no-write-allocate write-through lru < example_traces/swim.trace
+#./csim 256 4 16 write-allocate write-through lru < example_traces/swim.trace
+#./csim 256 4 16 write-allocate write-back lru < example_traces/read02.trace
+#./csim 2 4 16 write-allocate write-back lru < example_traces/read03.trace
+#./csim 256 4 16 write-allocate write-through lru < example_traces/write01.trace
+#./csim 256 4 16 write-allocate write-back lru < example_traces/write02.trace
 #./csim 256 4 16 write-allocate write-back lru < example_traces/gcc.trace
 
 
