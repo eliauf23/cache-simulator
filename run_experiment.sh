435743b2f6984bde39fcaf49e48dc02a8ca5d655
Elizabeth Aufzien <eaufzie1@jh.edu>
2021-11-02 13:50:09 -0400

    added shell script to output the 6 variations of cache for each cache config

---
+++ run_experiment.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+make clean
+make 
+echo "Let the hunger games begin: \n"
+
+./csim $1 $2 $3 write-allocate write-back fifo < example_traces/gcc.trace >> test_results/gcc_$1_$2_$3_writealloc_writeback_fifo.txt
+./csim $1 $2 $3 write-allocate write-through fifo < example_traces/gcc.trace >> test_results/gcc_$1_$2_$3_writealloc_writethru_fifo.txt
+./csim $1 $2 $3 no-write-allocate write-through fifo < example_traces/gcc.trace >> test_results/gcc_$1_$2_$3_nowritealloc_writethru_fifo.txt
+./csim $1 $2 $3 write-allocate write-back lru < example_traces/gcc.trace >> test_results/gcc_$1_$2_$3_writealloc_writeback_lru.txt
+./csim $1 $2 $3 write-allocate write-through lru < example_traces/gcc.trace >> test_results/gcc_$1_$2_$3_writealloc_writethru_lru.txt
+./csim $1 $2 $3 no-write-allocate write-through lru < example_traces/gcc.trace >> test_results/gcc_$1_$2_$3_nowritealloc_writethru_lru.txt
+
+./csim $1 $2 $3 write-allocate write-back fifo < example_traces/swim.trace >> test_results/swim_$1_$2_$3_writealloc_writeback_fifo.txt
+./csim $1 $2 $3 write-allocate write-through fifo < example_traces/swim.trace >> test_results/swim_$1_$2_$3_writealloc_writethru_fifo.txt
+./csim $1 $2 $3 no-write-allocate write-through fifo < example_traces/swim.trace >> test_results/swim_$1_$2_$3_nowritealloc_writethru_fifo.txt
+./csim $1 $2 $3 write-allocate write-back lru < example_traces/swim.trace >> test_results/swim_$1_$2_$3_writealloc_writeback_lru.txt
+./csim $1 $2 $3 write-allocate write-through lru < example_traces/swim.trace >> test_results/swim_$1_$2_$3_writealloc_writethru_lru.txt
+./csim $1 $2 $3 no-write-allocate write-through lru < example_traces/swim.trace >> test_results/swim_$1_$2_$3_nowritealloc_writethru_lru.txt
